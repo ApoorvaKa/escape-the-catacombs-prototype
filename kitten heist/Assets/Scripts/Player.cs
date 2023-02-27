08b31b26cef736b21fac83f195b39726a8931302
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-02-27 14:25:31 -0500

    Player Animation Added

--- kitten heist/Assets/Scripts/Player.cs
+++ kitten heist/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public float speed;
     public bool canMove = true;
     public bool isHiding = false;
+    public Animator animator;
 
     private Rigidbody2D rb;
     float horizontal, vertical;
@@ -23,6 +24,10 @@ public class Player : MonoBehaviour
             GetComponent<BoxCollider2D>().enabled = true;
             horizontal = Input.GetAxisRaw("Horizontal");
             vertical = Input.GetAxisRaw("Vertical");
+
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Speed", horizontal * horizontal + vertical * vertical);
+
             rb.velocity = new Vector2(horizontal, vertical).normalized * speed;
 
         } else {
